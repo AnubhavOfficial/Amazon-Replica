598da0834222c8c2924c17ada8bce8b3bc74dc0f
Anubhav <anubhavpanfer@gmail.com>
2022-09-14 22:07:54 +0530

    Checkout Success Page

--- src/Components/CheckoutSuccess.js
+++ src/Components/CheckoutSuccess.js
@@ -1,35 +1,51 @@
-import { Button, makeStyles, Typography } from "@material-ui/core";
+import { Button, Divider, makeStyles, Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import SetCartFromLocalStorageAction from "./../Actions/SetCartFromLocalStorageAction";
+import { FaCheckCircle } from "react-icons/fa";
+import orderPlaced from "../Assets/images/testing.png";
 
 const useStyles = makeStyles({
   main: {
-    marginTop: "3.5rem",
+    marginTop: "2.5rem",
     display: "flex",
-    height: "calc(100vh - 3.5rem)",
-    justifyContent: "center",
-    alignItems: "center",
+    height: "calc(100vh - 2.5rem)",
     flexDirection: "column",
+    // justifyContent: "center",
+    alignItems: "center",
+    background: "white",
+    padding: "1rem 2rem",
   },
   container: {
-    height: "85%",
-    width: "90%",
-    background: "silver",
+    height: "67%",
+    width: "99%",
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    background: "#ebebeb",
+    // justifyContent: "center",
+    // alignItems: "center",
     flexDirection: "column",
+    borderRadius: "0.3rem",
+    marginTop: "1.5rem",
+    padding: "1.3rem",
   },
-  checkout: {
-    fontSize: "2rem",
-    fontWeight: "bold",
+  box: {
+    background: "white",
+    padding: "1.5rem",
+    height: "calc(100% - 3rem)",
+    borderRadius: "0.3rem",
+    boxShadow: "0.5px 0.5px 3px 2px lightgrey",
+    display: "flex",
+  },
+  confirmation: {
+    fontSize: "1rem",
+    marginTop: "0.7rem",
   },
   buy: {
     background: "gold",
     fontSize: "1.2rem",
     padding: "0.5rem 2rem",
+    marginTop: "2rem",
     "&:hover": {
       background: "gold",
     },
@@ -37,11 +53,51 @@ const useStyles = makeStyles({
   link: {
     textDecoration: "none",
   },
+  orderPlaced: {
+    color: "#0d755e",
+    fontSize: "1.2rem",
+    fontWeight: "bold",
+    display: "flex",
+    alignItems: "center",
+  },
+  checkIcon: {
+    marginRight: "0.5rem",
+  },
+  shippingTo: {
+    fontWeight: "bold",
+    fontSize: "1rem",
+    marginTop: "1rem",
+  },
+  left: {
+    width: "50%",
+    // height: "100%",
+  },
+  right: {
+    width: "50%",
+    // height: "100%",
+    overflow: "none",
+  },
+  orderImage: {
+    width: "100%",
+    height: "100%",
+    borderRadius: "0.7rem",
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+  divider: {
+    margin: "0.7rem 0",
+    width: "98%",
+  },
+  date: {
+    color: "green",
+    fontWeight: "bold",
+  },
 });
 function CheckoutSuccess() {
   const dispatch = useDispatch();
   const signedIn = useSelector((state) => state.signedIn);
-  // const cartItems = useSelector((state) => state.cart.items);
+  const user = useSelector((state) => state.user);
 
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify({ items: [], count: 0 }));
@@ -49,16 +105,65 @@ function CheckoutSuccess() {
     dispatch(SetCartFromLocalStorageAction({ items: [], count: 0 }));
   }, [dispatch, signedIn]);
   const classes = useStyles();
+
+  const deliveryDate = () => {
+    let current = new Date();
+    current.setDate(current.getDate() + 5);
+    const date = current.getDate();
+    const month = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ][current.getMonth()];
+    const weekday = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ][current.getDay()];
+    return weekday + ", " + date + " " + month;
+  };
   return (
     <div className={classes.main}>
       <div className={classes.container}>
-        <Typography className={classes.checkout}>
-          Your order has been received
-        </Typography>
-        <Link to="/" className={classes.link}>
-          <Button className={classes.buy}>Continue Shopping</Button>
-        </Link>
+        <div className={classes.box}>
+          <div className={classes.left}>
+            <Typography className={classes.orderPlaced}>
+              <FaCheckCircle className={classes.checkIcon} />
+              Order placed, thank you!
+            </Typography>
+            <Typography className={classes.confirmation}>
+              Confirmation will be sent to your email.
+            </Typography>
+            <Typography className={classes.shippingTo}>
+              Shipping to {user.displayName}
+            </Typography>
+            <Divider className={classes.divider} />
+            <Typography className={classes.date}>{deliveryDate()}</Typography>
+            <Typography className={classes}>Delivery date</Typography>
+          </div>
+          <div className={classes.right}>
+            <a href="https://www.primevideo.com/" target="blank">
+              <img src={orderPlaced} alt="" className={classes.orderImage} />
+            </a>
+          </div>
+        </div>
       </div>
+      <Link to="/" className={classes.link}>
+        <Button className={classes.buy}>Continue Shopping</Button>
+      </Link>
     </div>
   );
 }
